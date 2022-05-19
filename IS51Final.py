360df6f5b45e9fd5d9f0472c38b4541bd4c0a8d2
badhesha <badhesha3@gmail.com>
2022-05-19 16:30:06 -0700

    Finish pseudo-code

--- IS51Final.py
+++ IS51Final.py
@@ -10,3 +10,22 @@ is 54.17%. The total number of grades is 24.
 End
 
 """
+"""
+pseudo-code
+
+Open "Final.txt"
+create a variable named grades to strip from list
+len(grades) #return length of list
+Initalize counter and sum to 0
+add the grade to the sum
+average function = sum(grades)/len(grades)
+Initalize counter and sum to 0
+num = 0 #for number of grades above average
+if grade > average
+num += 1
+(100*num)/len(grades) #format percentage
+print "Number of Grades"
+print "Average Grade"
+Print "Percentage of grades above average"
+
+"""
