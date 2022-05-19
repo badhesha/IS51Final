c6e87d56d6b6115b9519686ea0a3d248d380a73a
badhesha <badhesha3@gmail.com>
2022-05-19 16:26:06 -0700

    Finish structured-english

---
+++ IS51Final.py
@@ -0,0 +1,12 @@
+"""
+structured-english
+
+Start
+Download the text file titled Final.txt, which has the list of students grades.
+Next, create a function that will show the total number of grades, the average grades, and the percentage of grades
+that are above average. The final percentages the function should output for 
+average grade is 83.25%. The percentage for the grade above average 
+is 54.17%. The total number of grades is 24.
+End
+
+"""
